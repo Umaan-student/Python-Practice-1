20f570c0942ccfb08a177c0c839af6281fbb331e
Umaan-student <umantomail@gmail.com>
2025-10-29 16:01:38 +0530

    1st commit Upload python files to github

---
+++ Practice.py
@@ -0,0 +1,15 @@
+#Below code is practice set that sir send on Whatsapp group.
+name = "Amaan"      
+print("My Name is:",name)
+city = "Parbhani"  
+print("My city name is:", city)
+a = 10 
+b = 20
+print("Sum of NUmber is",a+b)
+message = "Welcome to The Python Programming"
+print(message)
+pi = 3.14
+print("Value of PI:", pi)
+print("2 + 3 =", 2 + 3)
+print("2 + 3 =", 2 + 3)
+
---
+++ first.py
@@ -0,0 +1,4 @@
+print("Hello, World")
+print("From Amaan Khan")
+name = "Amaan khan"
+# print(name)
