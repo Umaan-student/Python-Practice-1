650839e52470a60c35076a6c9f50826c5dd4dd33
Amaan-student <umantomail@gmail.com>
2025-11-21 14:18:29 +0530

    operators in python

---
+++ operator2.py
@@ -0,0 +1,14 @@
+a = 50
+b = 80
+c = a+b          #output 
+print("a+b:", c) #30
+c = a-b
+print("a-b:", c) #-30
+c = a*b
+print("a*b:", c) #4000
+c = a/b
+print("a/b:", c) #0.625
+c = a//b
+print("a//b:", c) #0
+c = a%b
+print("a%b:", c)  #50
