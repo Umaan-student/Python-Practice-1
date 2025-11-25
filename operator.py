1051e43160cb8beea0f2472ea6b3ab87e61de410
Amaan-student <umantomail@gmail.com>
2025-11-25 15:34:12 +0530

    upload some file

---
+++ operator.py
@@ -0,0 +1,17 @@
+# Arithmetic operators +-*/%//**
+#Relational operators >, >=,<,<=
+#equality operators ==,!=
+#Logical operators and or not
+#Bitwise operators &,|,^,~,<<,>>
+# & : bitwise AND= if both bit are 1 then only result is 1 otherwise resutl is 0.
+# | : bitwise OR= if atleast one bit is 1 then result is one otherwise is 0.
+# ^ : bitwise NOT= if bits are different then result is 1 other wise 0.
+# ~ : bitwise XOR= bitwise complement operators means 0 means 1 and 1 means 0.
+# >>: bitwise  Right Shift= bitwise left shift operators
+# << : bitwise Left Shift= bitwise right shift operators
+x = 7
+y = 10
+print("x binary is:",bin(x))
+print("y binary is:",bin(y))
+print(x|y)
+print(int(0b1111))
---
+++ ternary-operators.py
@@ -0,0 +1,9 @@
+# print("First Block of code")
+# a,b = 10,20
+# x=30 if a<b else 40
+# print("here a>b:",x)
+
+# print("secound Block of code")
+# a,b = 10,20
+# x=30 if a>b else 40
+# print("here a<b:",x)
